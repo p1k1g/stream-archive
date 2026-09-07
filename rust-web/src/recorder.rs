42733e7fd875a07c1b89370edaa5aed5f9d540b9
starft8900 <96937577+starft8900@users.noreply.github.com>
2026-09-07 14:12:44 +0900

    Fix RecorderConfig debug derive

--- rust-web/src/recorder.rs
+++ rust-web/src/recorder.rs
@@ -11,7 +11,7 @@ use tokio::{io::{AsyncBufReadExt, BufReader}, process::{Child, Command}};
 
 const GB: u64 = 1024 * 1024 * 1024;
 
-#[derive(Clone)]
+#[derive(Debug, Clone)]
 pub struct RecorderConfig {
     pub streamlink: PathBuf,
     pub quality: String,
